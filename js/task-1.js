02ae75d0ea51b6c0c2b8bccd4861aef79148c36e
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-09-16 18:50:57 +0300

    завершено

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,14 @@
-const menuQuantityEl = document.querySelectorAll("h2");
-console.log(`В списке ${menuQuantityEl.length} категории.`);
+// формує масив елементів категорії, масив
+const itemCat = document.querySelectorAll("#categories .item");
+
+// виводить загальну кількість категорій, число
+console.log(`В списке ${itemCat.length} категории.`);
+
+// перебирає елементи масиву
+itemCat.forEach((element) => {
+  // виводить назву категорії, текст
+  console.log(`Категория: ${element.querySelector("h2").textContent}`);
+
+  // виводить кількість елементів категорії, число
+  console.log(`Количество элементов: ${element.querySelectorAll("li").length}`);
+});
