35d69f60980a21ce25d2f4920f7a5c9356565de4
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-09-22 23:39:44 +0300

    додає зміну розвязку

--- js/task-2.js
+++ js/task-2.js
@@ -12,16 +12,13 @@ const ingredientsEl = document.querySelector("#ingredients");
 
 // перебираємо масив інгридієнтів
 const newListEl = ingredients.map((item) => {
-  // тимчасовий масив для нових елементів списку
-  const tempList = [];
+  // створюємо елемент
+  const newElement = document.createElement("li");
+  // додаємо текст елементу
+  newElement.textContent = item;
 
-  // створюємо елемент списку
-  const newListElement = document.createElement("li");
-  // призначаємо текст для елементу списку
-  newListElement.textContent = item;
-  // пушимо новостворений елемент до тимчасової колекції
-  tempList.push(newListElement);
-
-  //   додає нові елементи в список в одну операцію
-  ingredientsEl.appendChild(...tempList);
+  return newElement;
 });
+
+// додає нові елементи
+ingredientsEl.prepend(...newListEl);
